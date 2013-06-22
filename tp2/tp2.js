027ceb5a0bdb75ef6be876be7839a394865e9b10
Emmanuel Gautier <manu.gautier@sfr.fr>
2013-06-22 16:29:33 +0200

    Première mise en ligne du deuxième TP

---
+++ tp2/tp2.js
@@ -0,0 +1,19 @@
+$(function() {
+	var taphold = false;
+	
+	$( ".navbar ul li a" ).on( "click", function( event ) {
+		event.preventDefault();
+		
+		if(taphold){
+			taphold = false;
+		} else {
+			$.mobile.navigate( event.currentTarget.href );			
+		}
+	});
+	
+	$( ".navbar ul li a" ).on( 'taphold', function ( event ) {
+		taphold = true;
+		var tab = window.open("_blank");
+		tab.location = event.currentTarget.href;
+	});
+});
